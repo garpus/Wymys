9780f6758f8702d184a925ba43804a3c685d48d1
garpus <dragon11213@tlen.pl>
2017-04-23 11:08:24 +0200

    a
    a

--- my_gulp_101/docs/javascript/respo.js
+++ my_gulp_101/docs/javascript/respo.js
@@ -5,7 +5,7 @@ xhttp.onreadystatechange = function() {
     myFunction(this);
   }
 };
-xhttp.open("GET", "/files/tab.xml", true);
+xhttp.open("GET", "files/tab.xml", true);
 xhttp.send();
 }
 function myFunction(xml) {
